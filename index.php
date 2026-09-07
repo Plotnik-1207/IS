c24272b12710c18780f8bce1242093ae86f28c8a
Plotnik <artemplotnikov1207@gmail.com>
2026-09-07 19:35:48 +0700

    second

--- index.php
+++ index.php
@@ -1,8 +1,40 @@
 <?php
+class doc {
+    public $owner;
+    public $date;
+    public $cost;
+}
+function main() {
+    $array = [];
+    while(true){
+        $Doc = new doc();
+        $str = readline();
+        if ($str == "")
+            break;
+        $a = explode(' ', $str);
+        $Doc->date = array_pop($a);
+        $Doc->cost = array_pop($a);
+        $Doc->owner = trim(implode(' ', $a), '"');
+        $array[] = $Doc;
+    }
+
+}
+function m($array){
+    $count = 0;
+    foreach ($array as $i){
+        if(explode('.', $i->date)[1] == "07"){
+            $count++;
+        }
+    }
+}
+main();
+$Doc = new doc();
 echo "Введите строку\n";
 $s = readline();
 $a = explode(' ', $s);
-$d = null;
-$d["Владелец"] = $a[0];
-$d["Дата постановки на учет"] = $a[0];
-$d["Ориентировачная стоимость"] = $a[0];
+$Doc->date = array_pop($a);
+$Doc->cost = array_pop($a);
+$Doc->owner = trim(implode(' ', $a), '"');
+echo "Владелец: $Doc->owner\n";
+echo "Дата постановки на учет: $Doc->date\n";
+echo "Ориентировачная стоимость: $Doc->cost\n";
